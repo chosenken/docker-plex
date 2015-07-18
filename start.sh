0cfb26aadf31768c3a8108c443dc6f45ec39a866
Ken Herner <chosenken@gmail.com>
2015-07-18 19:07:32 -0400

    Parameterized start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -eux
 GROUP=plextmp
 
-mkdir -p /config/logs/supervisor
-#chown -R plex: /config
+CONFIGDIR=/config
+DATADIR=/mnt/Downloads
+USER=plex
+
+mkdir -p ${CONFIGDIR}/logs/supervisor
+chown -R ${USER} ${CONFIGDIR}
 
 touch /supervisord.log
 touch /supervisord.pid
-#chown plex: /supervisord.log /supervisord.pid
+chown ${USER}: /supervisord.log /supervisord.pid
 
-TARGET_GID=$(stat -c "%g" /data)
+TARGET_GID=$(stat -c "%g" ${DATADIR})
 EXISTS=$(cat /etc/group | grep ${TARGET_GID} | wc -l)
 
 # Create new group using target GID and add plex user
@@ -17,19 +21,13 @@ if [ $EXISTS = "0" ]; then
 else
   # GID exists, find group name and add
   GROUP=$(getent group $TARGET_GID | cut -d: -f1)
-  usermod -a -G ${GROUP} plex
+  usermod -a -G ${GROUP} ${USER}
 fi
-usermod -a -G ${GROUP} plex
-
-# Will change all files in directory to be readable by group
-if [ "${CHANGE_DIR_RIGHTS}" = true ]; then
-  chgrp -R ${GROUP} /data
-  chmod -R g+rX /data
-fi
+usermod -a -G ${GROUP} ${USER}
 
 # Current defaults to run as root while testing.
 if [ "${RUN_AS_ROOT}" = true ]; then
   /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
-  su plex -c "/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf"
+  su ${USER} -c "/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf"
 fi
